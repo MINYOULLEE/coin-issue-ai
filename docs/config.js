eb3adee6f618c7cf216164ff0d60f2b3a9c41643
린율 <kdauction087@gmail.com>
2026-08-21 03:02:26 +0700

    Add docs/config.js for local collector and Pages

---
+++ docs/config.js
@@ -0,0 +1,6 @@
+window.COIN_ISSUE_CONFIG = {
+  supabaseUrl: "https://ljazcstmwtuhideaarti.supabase.co",
+  supabaseAnonKey: "sb_publishable_a-LZ7__bpIa6j8C7Bjdr1Q_aQJJbdVz",
+  refreshSeconds: 5,
+  staleAfterSeconds: 45
+};
