f4ab6f1d47a4d92f9257dd6f6e2ec9b9abd983eb
린율 <kdauction087@gmail.com>
2026-08-21 17:46:18 +0700

    Align dashboard freshness with cloud cron interval

--- docs/config.js
+++ docs/config.js
@@ -2,5 +2,5 @@ window.COIN_ISSUE_CONFIG = {
   supabaseUrl: "https://ljazcstmwtuhideaarti.supabase.co",
   supabaseAnonKey: "sb_publishable_a-LZ7__bpIa6j8C7Bjdr1Q_aQJJbdVz",
   refreshSeconds: 5,
-  staleAfterSeconds: 45
+  staleAfterSeconds: 150
 };
